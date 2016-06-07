c34c83bc3c70d5e2d1f4b91da95cf71f8384ca06
Yaroslav Konoplov <yaroslav@inbox.com>
2016-06-07 19:54:48 +0300

    ruby 2.3 frozen string literal

--- lib/activerecord-autostrip.rb
+++ lib/activerecord-autostrip.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'active_support/all'
 require 'active_record'
 require 'unicode-tools'
--- lib/activerecord-autostrip/extension.rb
+++ lib/activerecord-autostrip/extension.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Autostrip
   module Extension
     extend ActiveSupport::Concern
